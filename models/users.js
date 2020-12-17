d2eb6130e4d2592a1331b7077bd3f00261959e3f
Maxime Verdy <maximeverdy@gmail.com>
2020-12-17 18:29:03 +0100

    chiffrement en place et optimisation des messages d'erreur de login

--- models/users.js
+++ models/users.js
@@ -2,7 +2,9 @@ var mongoose = require('mongoose')
 
 const userSchema = mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    token: String,
+    salt: String, 
 })
 
 var userModel = mongoose.model('users', userSchema)
