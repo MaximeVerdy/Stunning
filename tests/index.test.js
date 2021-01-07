074678edf4496fa1782e97de1d105e5ff24ec7cf
Maxime Verdy <maximeverdy@gmail.com>
2021-01-07 22:03:35 +0100

    détail mentions légales, + de code commenté, ajout de tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,45 +1,103 @@
-var app = require("../app")
-var request = require("supertest")
+var app = require("../app") // Lien vers le serveur
+var request = require("supertest") //  importe du module supertest
 
-describe("route des activités enregistrées en BDD", () => { 
+// NOTA BENE :le groupe de test suivant 'activités en BDD pour 1 utilisateur inscrit' 
+// ne respecte pas totalement la processure normale TDD
+// Dans le cadre de l'examen, le goupe TDD suivant a été découpé 
+// de manière à illustrer les détails de la réflexion.
+// C'est à dire qu'au lien de réécrire le même test à chaque fois qu'il passe (refactoring),
+// l'étape de développement suivante a été mise dans le test d'après
+// C'est le dernier test du groupe qui refactor l'ensemble
 
-test("des données en BDD existent pour utilisateur spécifique", async (done) => {
-await request(app).get("/historytest")
-  .query({ "token" : "tokenTest01"})
-  .expect(200)
-  .expect(
-    { activities: [
-      {
-        _id: '5ff33915123d441b61d6f697',
-        token: 'tokenTest01',
-        activityID: 'FJjpbKBTVDauZVEFNj6YcT4LV6sbZc2Y',
-        distance: 10,
-        date: '2021-01-10T00:00:00.000Z',
-        chronoH: 0,
-        chronoM: 50,
-        chronoS: 0,
-        type: 'varié',
-        __v: 0
-      }
-    ], 
-      error: [] }
-)
-done()
-});
-}); 
-
-test("quand utilisateur veut accéder aux données sans être correctement identifié", async (done) => {
-  await request(app).get("/historytest")
-    .query({ "token" : "tokenTestFaux"})
-    .expect(200)
-    .expect(
-      { activities: [], 
-        error: ['Veuillez vous connecter'] }
-  )
+describe('activités en BDD pour 1 utilisateur inscrit', () => { // groupe de test
+  test("succès de la requête sur la bonne route", async (done) => { // test 1
+  await request(app).get("/historytest") // asynchronie de la requête
+    .expect(200) // attendu : succès de la requête
   done()
   });
 
-test("suppression des documents en BDD", async (done) => {
+  test("envoyer les données si l'utisateur est inscrit", async (done) => { // test 2
+    await request(app).get("/historytestb") // asynchronie de la requête
+      .query({ "token" : "tokenTest"}) // paramètre de la requête avec champ et valeur
+      .expect({
+        token : "tokenTest"
+      })
+    done()
+  });
+
+  test("existance de données en BDD", async (done) => { // test 3
+    await request(app).get("/historytest") // asynchronie de la requête
+      .expect({
+        activityExist : true // attendu :  qu'il y est des données en BDD
+      })
+    done()
+  });
+
+  test("existance de données en BDD pour 1 utilisateur précis", async (done) => { // test 4
+    await request(app).get("/historytest") // asynchronie de la requête
+      .query({ "token" : "tokenTest01"}) // paramètre de la requête avec champ et valeur
+      .expect({
+        activityExist : true // attendu :  qu'il y est des données en BDD
+      })
+    done()
+  });
+
+  test("les bonnes données correspondent au bon utilisateur", async (done) => { // test 5
+    await request(app).get("/historytest2") // asynchronie de la requête
+      .query({ "token" : "tokenTest"}) // paramètre de la requête avec champ et valeur
+      .expect({
+        activities: [
+          {
+            _id: '5ff37d7eacdb6b3ea0a9eb63',
+            token: 'tokenTest',
+            activityID: 'FJjpbKBTcT4LV6sbZc2Y',
+            distance: 10,
+            date: '2021-01-10T00:00:00.000Z',
+            chronoH: 0,
+            chronoM: 50,
+            chronoS: 0,
+            type: 'varié',
+            __v: 0
+          }
+        ]
+      })
+    done()
+  });
+
+  test("envoyer les données si l'utisateur est inscrit", async (done) => { // test 6
+    await request(app).get("/historytest3") // asynchronie de la requête
+      .query({ "token" : "tokenTest"}) // paramètre de la requête avec champ et valeur
+      .expect({
+        activities: [
+          {
+            _id: '5ff37d7eacdb6b3ea0a9eb63',
+            token: 'tokenTest',
+            activityID: 'FJjpbKBTcT4LV6sbZc2Y',
+            distance: 10,
+            date: '2021-01-10T00:00:00.000Z',
+            chronoH: 0,
+            chronoM: 50,
+            chronoS: 0,
+            type: 'varié',
+            __v: 0
+          }
+        ]
+      })
+    done()
+  });
+
+  test("envoi d'un message d'erreur en cas non inscription", async (done) => { // test 7
+    await request(app).get("/historytest4") // asynchronie de la requête
+      .query({ "token" : "tokenTest"}) // paramètre de la requête avec champ et valeur
+      .expect({
+        activities: [],
+        error : [ 'Veuillez vous connecter' ]
+      })
+    done()
+  });
+})
+
+test("suppression des documents en BDD", async (done) => { // test 8
   await request(app).delete("/historytest")
     .send({ "activityID" : "activityIDtest2"})
     .expect(200)
@@ -49,7 +107,7 @@ test("suppression des documents en BDD", async (done) => {
   done()
   });
 
-test("écrire d'un document en BDD", async (done) => {
+test("écrire d'un document en BDD", async (done) => { // test 9
   await request(app).post("/saveactivitytest")
     .send({"tokenFromFront" : "tokenTest01",
             "distanceFromFront" : 100,
@@ -76,22 +134,22 @@ test("écrire d'un document en BDD", async (done) => {
               }
             ]
      })
-    //  .toEqual({expect.objectContaining({ 
-    //    _id: expect.any(String),
-    //    token: 'tokenTest01',
-    //    activityID: expect.any(String),
-    //    distance: 100,
-    //    date: '2021-01-10T00:00:00.000Z',
-    //    chronoH: 24,
-    //    chronoM: 1,
-    //    chronoS: 2,
-    //    type: 'varié',
-    //    __v: 0
-    //  })})
+            //  .toEqual({expect.objectContaining({ 
+            //    _id: expect.any(String),
+            //    token: 'tokenTest01',
+            //    activityID: expect.any(String),
+            //    distance: 100,
+            //    date: '2021-01-10T00:00:00.000Z',
+            //    chronoH: 24,
+            //    chronoM: 1,
+            //    chronoS: 2,
+            //    type: 'varié',
+            //    __v: 0
+            //  })})
   done()
   });
 
-test("affichages des données stats", async (done) => {
+test("affichages des données stats", async (done) => { // test 10
 await request(app).get("/stats")
   .query({ "token" : "tokenTest01",
           "yearChosen" : 121,
@@ -190,9 +248,7 @@ await request(app).get("/stats")
 done()
 });
 
-
-
-test("inscription d'un utilisateur", async (done) => {
+test("inscription d'un utilisateur", async (done) => { // test 11
 await request(app).post("/sign-up-test")
   .send({ "emailFromFront" : "emailOK@gmail.com",
           "passwordFromFront" : "mdpOK#1111111111"
@@ -205,10 +261,7 @@ await request(app).post("/sign-up-test")
 done()
 });
 
-
-
-
-test("reconnexion d'un utilisateur", async (done) => {
+test("reconnexion d'un utilisateur", async (done) => { // test 12
   await request(app).post("/sign-in-test")
     .send({ "emailFromFront" : "emailOK@gmail.com",
             "passwordFromFront" : "mdpMauvais"
